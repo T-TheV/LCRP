e587f8b736ca51f930823e25174b3290f67813c0
TheV <djardim322@gmail.com>
2025-05-12 21:32:01 -0300

    - Adicionados arquivos TypeScript iniciais para o módulo ATM nos diretórios de clientes e servidores. - Criadas definições de tipo necessárias e arquivos de configuração. - Configurados componentes básicos para a interface ATM e funcionalidades relacionadas.

---
+++ ragemp-typescript/src/client/@types/rage-mp-client/index.d.ts
@@ -0,0 +1,5 @@
+Feat: Inicializado o módulo ATM com estrutura e tipos básicos
+
+- Adicionados arquivos TypeScript iniciais para o módulo ATM nos diretórios de clientes e servidores.
+- Criadas definições de tipo necessárias e arquivos de configuração.
+- Configurados componentes básicos para a interface ATM e funcionalidades relacionadas.
